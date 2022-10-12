cb2bf0304b6a1cc8d00b36b04e971fe3495828ee
jarbas.martinho <jarbasaugusto@gmail.com>
2022-10-12 11:47:33 -0300

    Final da Aula

---
+++ 03-minibrowser/map.js
@@ -0,0 +1,15 @@
+//TDD - Test Driven Developement 
+
+var novoMapa = new Map();
+
+class HistoryMap {
+
+
+}
+
+novoMapa.set('11/11/2011 - 23:30', { title: 'Titulo do Site', url: 'url do site'});
+novoMapa.set('11/11/2011 - 23:30', {});
+
+var resultado = novoMapa;
+
+console.log(resultado);
